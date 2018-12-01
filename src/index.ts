ab7dfb2f594a6ebb469e1982b0a4c5eea142da84
Max Milton <max@wearegenki.com>
2018-12-02 08:53:01 +1100

    Add simple placeholder code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-// TODO
+// TODO: Remove this placeholder code
+
+import packageJson from '../package.json';
+
+/* tslint:disable-next-line no-console */
+console.log('Version', packageJson.version);
