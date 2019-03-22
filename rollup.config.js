d91cb3b24da2e0b8726ce61198be3396740361a2
Max Milton <max@wearegenki.com>
2019-03-23 03:38:46 +1100

    Improve build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
+/* eslint-disable global-require */
+
+import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'dist',
+    dir: 'lib',
     format: 'esm',
     sourcemap: true,
   },
   plugins: [
+    resolve(),
     typescript({
       typescript: require('typescript'),
     }),
